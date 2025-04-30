1fe2837dccd51061f0eae10329dbd32afd608d8f
pribishlove <m.makushkin2004@gmail.com>
2025-04-30 16:46:46 +0500

    fixed linter errors

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,27 +1,27 @@
-import { Command } from "./command.interface.js";
+import { Command } from './command.interface.js';
 import chalk from 'chalk';
 import { TSVFileReader } from '../../shared/libs/file-reader/tsv-file-reader.js';
 
 export class ImportCommand implements Command {
-    public getName() {
-        return '--import';
+  public getName() {
+    return '--import';
+  }
+
+  public execute(...params: string[]): void {
+    const [filename] = params;
+    const fileReader = new TSVFileReader(filename.trim());
+
+    try {
+      fileReader.read();
+      const resultArray = fileReader.toArray();
+      console.log(chalk.red(JSON.stringify(resultArray, null, 2)));
+    } catch (error) {
+      if (!(error instanceof Error)) {
+        throw error;
+      }
+
+      console.error('Failed to import the file');
+      console.error(`Error: ${error.message}`);
     }
-  
-    public execute(...params: string[]): void {
-        const [filename] = params;
-        const fileReader = new TSVFileReader(filename.trim());
-  
-        try {
-            fileReader.read();
-            const resultArray = fileReader.toArray();
-            console.log(chalk.red(JSON.stringify(resultArray, null, 2)));
-        } catch (error) {
-            if (!(error instanceof Error)) {
-                throw error;
-            }
-    
-            console.error('Failed to import the file')
-            console.error(`Error: ${error.message}`);
-        }
-    }
+  }
 }
